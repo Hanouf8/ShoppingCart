1280d6ceeadb7ec720815c8c4c6fafde03b288aa
Hanouf8 <147676497+Hanouf8@users.noreply.github.com>
2024-04-21 00:19:53 +0300

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -9,62 +9,54 @@
  */
 public class ShoppingCart {
     
-private Item[] items;
-private int itemCount;
-private double totalPrice;
+    private ItemFactory itemFactory; // Factory for creating items
+    private List<Item> items; // List of items in the shopping cart
+    private double totalPrice; // Total price of the items
+    private static ShoppingCart instance; // Singleton instance of the shopping cart
 
+    private ShoppingCart(ItemFactory itemFactory) {
+        this.itemFactory = itemFactory;
+        items = new ArrayList<>();
+        totalPrice = 0.0;
+    }
 
+    public static ShoppingCart getInstance(ItemFactory itemFactory) {
+        if (instance == null) {
+            instance = new ShoppingCart(itemFactory); // Create a new instance if it doesn't exist
+        }
+        return instance; // Return the existing instance
+    }
 
-public ShoppingCart() {
-    items = new Item[10];
-    itemCount = 0;
-    totalPrice = 0.0;
-}
+    public void addItem(String name, double price) {
+        Item item = itemFactory.createItem(name, price); // Create a new item using the factory
+        items.add(item); // Add the item to the list
+        totalPrice += item.getPrice(); // Update the total price
+    }
 
-public void addItem(Item item) {
-    items[itemCount] = item;
-    itemCount++;
-    totalPrice += item.getPrice();
-}
-
-public void removeItem(Item item) {
-    for (int i = 0; i < itemCount; i++) {
-        if (items[i].equals(item)) {
-            totalPrice -= item.getPrice();
-            items[i] = null;
-            itemCount--;
-            condenseItems();
-            break;
+    public void removeItem(Item item) {
+        if (items.remove(item)) {
+            totalPrice -= item.getPrice(); // Remove the item from the list and update the total price
         }
     }
-}
 
-private void condenseItems() {
-    int j = 0;
-    for (int i = 0; i < itemCount; i++) {
-        if (items[i] != null) {
-            items[j] = items[i];
-            j++;
+    public double calculateTotalPrice() {
+        return totalPrice; // Return the total price of the items
+    }
+
+    public void pay(String paymentMethod) {
+        if (paymentMethod.equals("CreditCard")) {
+            System.out.println("Processing credit card payment: $" + calculateTotalPrice());
+        } else if (paymentMethod.equals("PayPal")) {
+            System.out.println("Processing PayPal payment: $" + calculateTotalPrice());
+        } else {
+            System.out.println("Invalid payment method");
         }
     }
-}
 
-public double calculateTotalPrice() {
-    return totalPrice;
-}
-
-public void pay(String paymentMethod) {
-    if (paymentMethod.equals("CreditCard")) {
-        // Process credit card payment
-        System.out.println("Processing credit card payment: $" + calculateTotalPrice());
-    } else if (paymentMethod.equals("PayPal")) {
-        // Process PayPal payment
-        System.out.println("Processing PayPal payment: $" + calculateTotalPrice());
-    } else if (paymentMethod.equals("Bitcoin")) {
-        // Process Bitcoin payment
-        System.out.println("Processing Bitcoin payment: $" + calculateTotalPrice());
-    } else {
-        System.out.println("Invalid payment method");
+    public int calculateFactorial(int number) {
+        if (number == 0) {
+            return 1; // Base case: factorial of 0 is 1
+        } else {
+            return number * calculateFactorial(number - 1); // Recursive case: calculate factorial using recursion
+        }
     }
-}
-}
