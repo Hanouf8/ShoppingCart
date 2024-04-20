c4bfcf18cb810055cfe57f44ccdddc5381c95ba8
Hanouf8 <147676497+Hanouf8@users.noreply.github.com>
2024-04-21 00:21:08 +0300

    Update ShoppingCartApp.java

--- ShoppingCartApp.java
+++ ShoppingCartApp.java
@@ -13,14 +13,22 @@ public class ShoppingCartApp {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        ShoppingCart shoppingCart = new ShoppingCart();
-Item item1 = new Item("Item 1", 10.0);
-Item item2 = new Item("Item 2", 20.0);
-shoppingCart.addItem(item1);
-shoppingCart.addItem(item2);
-shoppingCart.pay("CreditCard");
-}
+     public static void main(String[] args) {
+        ItemFactory itemFactory = new SimpleItemFactory(); // Create an instance of the item factory
+        ShoppingCart shoppingCart = ShoppingCart.getInstance(itemFactory); // Get the instance of the shopping cart
+
+        shoppingCart.addItem("Item 1", 10.0); // Add items to the shopping cart
+        shoppingCart.addItem("Item 2", 20.0);
+        shoppingCart.pay("CreditCard"); // Pay with credit card
+        
+        ShoppingCart shoppingCart2 = ShoppingCart.getInstance(itemFactory); // Get the existing instance of the shopping cart
+        shoppingCart2.addItem("Item 3", 15.0); // Add items to the shopping cart
+        shoppingCart2.pay("PayPal"); // Pay with PayPal
+        
+        int number = 5;
+        int factorial = shoppingCart.calculateFactorial(number); // Calculate the factorial of a number
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
 }
     
     
