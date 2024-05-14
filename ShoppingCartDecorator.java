85869dc26b814ca6ff7653a98d4da5f3193ffbf1
Hanan <114772570+Hzzxjj@users.noreply.github.com>
2024-05-14 10:31:54 +0300

    Update ShoppingCartDecorator.java

--- ShoppingCartDecorator.java
+++ ShoppingCartDecorator.java
@@ -23,3 +23,18 @@ public abstract class ShoppingCartDecorator implements ShoppingCart {
         return decoratedShoppingCart.calculateFactorial(number);
     }
 }
+
+public class DiscountedShoppingCartDecorator extends ShoppingCartDecorator {
+    private double discountRate;
+
+    public DiscountedShoppingCartDecorator(ShoppingCart decoratedShoppingCart, double discountRate) {
+        super(decoratedShoppingCart);
+        this.discountRate = discountRate;
+    }
+
+    @Override
+    public double calculateTotalPrice() {
+        double totalPrice = decoratedShoppingCart.calculateTotalPrice();
+        return totalPrice * (1 - discountRate); // Apply the discount
+    }
+}
