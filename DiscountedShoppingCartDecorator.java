67931f8231f89deb4c39e6462f3f519416bf7ff8
Hanouf8 <147676497+Hanouf8@users.noreply.github.com>
2024-05-14 06:41:55 +0300

    Update DiscountedShoppingCartDecorator.java

--- DiscountedShoppingCartDecorator.java
+++ DiscountedShoppingCartDecorator.java
@@ -1,30 +1,15 @@
-public class DiscountedShoppingCartDecorator implements ItemFactory {
-    private ItemFactory itemFactory; // Reference to the original ItemFactory
-    private double discountRate; // Discount rate
 
-    public DiscountedShoppingCartDecorator(ItemFactory itemFactory, double discountRate) {
-        this.itemFactory = itemFactory;
+public class DiscountedShoppingCartDecorator extends ShoppingCartDecorator {
+    private double discountRate;
+
+    public DiscountedShoppingCartDecorator(ShoppingCart decoratedShoppingCart, double discountRate) {
+        super(decoratedShoppingCart);
         this.discountRate = discountRate;
     }
 
     @Override
-    public Item createItem(String name, double price) {
-        return itemFactory.createItem(name, price); // Delegate the item creation to the original ItemFactory
-    }
-
-    @Override
     public double calculateTotalPrice() {
-        double totalPrice = itemFactory.calculateTotalPrice();
+        double totalPrice = decoratedShoppingCart.calculateTotalPrice();
         return totalPrice * (1 - discountRate); // Apply the discount
-    }
-
-    @Override
-    public void pay(String paymentMethod) {
-        itemFactory.pay(paymentMethod); // Delegate the payment operation to the original ItemFactory
-    }
-
-    @Override
-    public int calculateFactorial(int number) {
-        return itemFactory.calculateFactorial(number); // Delegate the factorial calculation to the original ItemFactory
     }
 }
